f6fcc767659012448538657e84e5bf99c3a617dc
Mes-keb <omeskebi15@gmail.com>
2026-09-12 12:29:25 -0600

    form

---
+++ form.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Create window
+window = tk.Tk()
+window.title("Registration form")
+window.geometry("400x450")
+window.resizelble(False, False)
