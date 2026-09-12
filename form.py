899469421e10e97f55c983c2ce94276da074bd26
Mes-keb <omeskebi15@gmail.com>
2026-09-12 12:35:36 -0600

    form

--- form.py
+++ form.py
@@ -6,3 +6,14 @@ window = tk.Tk()
 window.title("Registration form")
 window.geometry("400x450")
 window.resizelble(False, False)
+
+# Title
+title = tk.Label(
+    window,
+    text="Registration Form",
+    font=("Ariel", 20, "bold")
+)
+title.pack(pady=20)
+
+# Full name
+tk.Label(window, text="Full Name").pack(ancor="w", padx=50)
